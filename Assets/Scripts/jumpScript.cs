8d484e34f7b010f0760064a48291ed549e0a7348
JPassalacqua97 <JVPass@gmail.com>
2020-10-14 17:03:46 -0400

    Adding Jump Script and test screen
    Added a script that makes the user able to jump. While the player is on top of an object they should be able to jump again. Whle the player is in mid air they should not be able to jump.

---
+++ Assets/Scripts/jumpScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpScript : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private BoxCollider cl;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cl = GetComponent<BoxCollider>();
+    }
+    public bool isGrounded = false;
+    public float jumpHeight = 8f;
+    public int count;
+    void Update()
+    {
+            
+        
+       if(Input.GetKeyDown(KeyCode.Space) && isGrounded){  
+               rb.AddForce(Vector3.up * jumpHeight);
+               isGrounded = false;           
+               Debug.Log("Jump"); 
+        }
+    }
+    void OnCollisionEnter(Collision theCollision){
+        if(theCollision.gameObject.tag == "Jump"){
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(){
+
+    }
+}
