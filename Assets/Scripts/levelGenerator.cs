9e1660028bc91d8ce8e815affb431a5b3d599013
JPassalacqua97 <JVPass@gmail.com>
2020-10-14 20:47:04 -0400

    Object Generator
    Created a script that automatically generates an object at an x,y position, fixed collider bug in jump scene

---
+++ Assets/Scripts/levelGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelGenerator : MonoBehaviour
+{
+  [SerializeField] private Transform Object_Generator;
+
+
+  private void Awake(){
+      generateLevelObstacle(new Vector3(60,2,0));
+  }
+
+  private void generateLevelObstacle(Vector3 spawnPosition){
+     Instantiate(Object_Generator, spawnPosition, Quaternion .identity);
+  }
+
+  //private void generate(){
+   //   Instantiate(Obstacle_Generator, new Vector3(194,95,-64), Quaternion .identity);
+  ///}
+}
