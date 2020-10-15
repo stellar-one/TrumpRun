1e51854d83ddf04993bd2a9a1c3307eea65597ef
JPassalacqua97 <JVPass@gmail.com>
2020-10-15 19:28:12 -0400

    Started developing script for level generator
    In  progress getting it to work.

--- Assets/Scripts/levelGenerator.cs
+++ Assets/Scripts/levelGenerator.cs
@@ -6,19 +6,42 @@ using UnityEngine;
 public class levelGenerator : MonoBehaviour
 {
   [SerializeField] private Transform Object_Generator;
+ 
+  public Transform lastObj;  
 
-  private void Awake(){
-    int z = Random.Range(-5,4);
-    int x = Random.Range(20,60);
-    while ( x < 500) {
-      generateLevelObstacle(new Vector3(x,2,z));
-      x += Random.Range(5,45);
-      z = Random.Range(-5,4);  
-    }
+  void Start(){
+      Transform[] childs = Object_Generator.GetComponentsInChildren<Transform>();
+      Transform randomObject = childs[0];
+      float y = 0;
+      float z = 0;
+      float x = 520;
+      lastObj = randomObject; 
+      generateLevelObstacle(new Vector3(x,y,z), randomObject);
+      
   }
 
-  private void generateLevelObstacle(Vector3 spawnPosition){
-     Instantiate(Object_Generator, spawnPosition, Quaternion .identity);
+  private void waitFiveSec(){
+  }
+   void Update(){
+    Transform[] childs = Object_Generator.GetComponentsInChildren<Transform>();
+    
+ 
+      Transform randomObject = childs[0];                                                 //Random.Range(0,childs.Length)
+      if(lastObj.transform.position.x < 450){                                            // try to use yield or a timer make lastObj a reference
+        float y = 0;
+        float z = 0;
+        float x = 520;
+        lastObj = randomObject; 
+        generateLevelObstacle(new Vector3(x,y,z), randomObject); 
+        Debug.Log(lastObj.position.x);
+      } 
+  
+  }
+
+  
+
+  private void generateLevelObstacle(Vector3 spawnPosition, Transform randomObject){
+     Instantiate(randomObject, spawnPosition, Quaternion.identity);    //.getChidCount
   }
 
 }
