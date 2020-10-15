3465f667bf43a768713614e72a9012b6a43e2316
JPassalacqua97 <JVPass@gmail.com>
2020-10-15 00:21:08 -0400

    Level Generator Script
    Script that generates objects throught the level. Beginning stages. Issues need to be worked on still

--- Assets/Scripts/levelGenerator.cs
+++ Assets/Scripts/levelGenerator.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class levelGenerator : MonoBehaviour
 {
   [SerializeField] private Transform Object_Generator;
 
-
   private void Awake(){
-      generateLevelObstacle(new Vector3(60,2,0));
+    int z = Random.Range(-5,4);
+    int x = Random.Range(20,60);
+    while ( x < 500) {
+      generateLevelObstacle(new Vector3(x,2,z));
+      x += Random.Range(5,45);
+      z = Random.Range(-5,4);  
+    }
   }
 
   private void generateLevelObstacle(Vector3 spawnPosition){
      Instantiate(Object_Generator, spawnPosition, Quaternion .identity);
   }
 
-  //private void generate(){
-   //   Instantiate(Obstacle_Generator, new Vector3(194,95,-64), Quaternion .identity);
-  ///}
 }
