11439aa4bbbda4851a59fecef279b276468dde30
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-10-16 01:45:29 -0400

    coroutines for spawning obstacles... need spawn positions fixed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,29 +5,69 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-  Transform playerPosition;
+  GameObject player;
   GameObject[] obstacles;
-  GameObject endPoint;
+  // GameObject endPoint;
 
   void Awake(){
-    playerPosition = GameObject.FindWithTag("Player").transform.GetChild(1);
+    player = GameObject.FindWithTag("Player").transform.GetChild(0).gameObject;
     obstacles = GameObject.FindGameObjectsWithTag("Enemy");
-    endPoint = GameObject.FindWithTag("Finish");
-  }
-  
-  void Update(){
-    if(playerPosition.transform.position.x == -endPoint.transform.position.x - 300){
-      generatePath();
-    }
+    // endPoint = GameObject.FindWithTag("Finish");
   }
 
-  private void generatePath(){
-    GameObject randomObj = obstacles[Random.Range(0,obstacles.Length)];
-    generateLevelObstacle(new Vector3(0, 0, 520), randomObj); 
+  void Start(){
+    StartCoroutine(SpawnObstaclesCoroutine(new Vector3(0f, 1f, 1f), obstacles[0]));
+    StartCoroutine(SpawnObstaclesCoroutine(new Vector3(1f, 2f, 2f), obstacles[0]));
+    StartCoroutine(SpawnObstaclesCoroutine(new Vector3(2f, 3f, 3f), obstacles[0]));
+    // StartCoroutine(SpawnObstaclesCoroutine(new Vector3(Random.Range(player.transform.localPosition.z, player.transform.localPosition.z + 50f), 2f, 2f), obstacles[1]));
+    // StartCoroutine(SpawnObstaclesCoroutine(new Vector3(Random.Range(player.transform.localPosition.z, player.transform.localPosition.z + 50f), 3f, 3f), obstacles[2]));
   }
   
-  private void generateLevelObstacle(Vector3 spawnPosition, GameObject obstacle){
-    Instantiate(obstacle, spawnPosition, Quaternion.identity);
-  }
+  // void Update(){
+  //   if(player.transform.localPosition.z > 200f){
+  //     generatePath();
+  //   }
+  // }
+
+  // private void generatePath(){
+  //   GameObject randomObj = obstacles[Random.Range(0,obstacles.Length)];
+  //   // float x = Random.Range(player.transform.localPosition.z + 50f, player.transform.localPosition.z + 100f);
+
+  //   if(randomObj.name == "Floating"){
+  //     generateLevelObstacle(new Vector3(10f, 3f, 1f), randomObj); 
+  //   }
+  //   if(randomObj.name == "Cube"){
+  //     generateLevelObstacle(new Vector3(25f, 2f, 2f), randomObj); 
+  //   }
+  //   if(randomObj.name == "Rectangle"){
+  //     generateLevelObstacle(new Vector3(50f, 2f, 3f), randomObj); 
+  //   }
+    
+  // }
+
+  // private void generatePath(){
+  //   GameObject randomObj = obstacles[Random.Range(0,obstacles.Length)];
+  //   // float x = Random.Range(player.transform.localPosition.z + 50f, player.transform.localPosition.z + 100f);
+
+  //   if(randomObj.name == "Floating"){
+  //     SpawnObstaclesCoroutine(new Vector3(10f, 3f, 1f), randomObj); 
+  //   }
+  //   if(randomObj.name == "Cube"){
+  //     SpawnObstaclesCoroutine(new Vector3(25f, 2f, 2f), randomObj); 
+  //   }
+  //   if(randomObj.name == "Rectangle"){
+  //     SpawnObstaclesCoroutine(new Vector3(50f, 2f, 3f), randomObj); 
+  //   }
+    
+  // }
+
+  IEnumerator SpawnObstaclesCoroutine (Vector3 spawnPosition, GameObject obstacle)
+    {
+        WaitForSeconds waitTime = new WaitForSeconds(2);
+        while (true) {
+            Instantiate(obstacle.transform, spawnPosition, Quaternion.identity);
+            yield return waitTime;
+        }
+    }
 
 }
