7c884f3daa8bcbaa753e8a05f6fde8991f86a42c
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-10-16 01:48:13 -0400

    code formatting and renamed scene to blackout

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,44 +22,6 @@ public class GameManager : MonoBehaviour
     // StartCoroutine(SpawnObstaclesCoroutine(new Vector3(Random.Range(player.transform.localPosition.z, player.transform.localPosition.z + 50f), 2f, 2f), obstacles[1]));
     // StartCoroutine(SpawnObstaclesCoroutine(new Vector3(Random.Range(player.transform.localPosition.z, player.transform.localPosition.z + 50f), 3f, 3f), obstacles[2]));
   }
-  
-  // void Update(){
-  //   if(player.transform.localPosition.z > 200f){
-  //     generatePath();
-  //   }
-  // }
-
-  // private void generatePath(){
-  //   GameObject randomObj = obstacles[Random.Range(0,obstacles.Length)];
-  //   // float x = Random.Range(player.transform.localPosition.z + 50f, player.transform.localPosition.z + 100f);
-
-  //   if(randomObj.name == "Floating"){
-  //     generateLevelObstacle(new Vector3(10f, 3f, 1f), randomObj); 
-  //   }
-  //   if(randomObj.name == "Cube"){
-  //     generateLevelObstacle(new Vector3(25f, 2f, 2f), randomObj); 
-  //   }
-  //   if(randomObj.name == "Rectangle"){
-  //     generateLevelObstacle(new Vector3(50f, 2f, 3f), randomObj); 
-  //   }
-    
-  // }
-
-  // private void generatePath(){
-  //   GameObject randomObj = obstacles[Random.Range(0,obstacles.Length)];
-  //   // float x = Random.Range(player.transform.localPosition.z + 50f, player.transform.localPosition.z + 100f);
-
-  //   if(randomObj.name == "Floating"){
-  //     SpawnObstaclesCoroutine(new Vector3(10f, 3f, 1f), randomObj); 
-  //   }
-  //   if(randomObj.name == "Cube"){
-  //     SpawnObstaclesCoroutine(new Vector3(25f, 2f, 2f), randomObj); 
-  //   }
-  //   if(randomObj.name == "Rectangle"){
-  //     SpawnObstaclesCoroutine(new Vector3(50f, 2f, 3f), randomObj); 
-  //   }
-    
-  // }
 
   IEnumerator SpawnObstaclesCoroutine (Vector3 spawnPosition, GameObject obstacle)
     {
