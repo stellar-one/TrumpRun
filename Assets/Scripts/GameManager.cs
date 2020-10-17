2b5f30d91233226a929529b3b9f57f97a8df24e0
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-10-16 23:24:49 -0400

    testing some ideas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,22 +10,23 @@ public class GameManager : MonoBehaviour
   // GameObject endPoint;
 
   void Awake(){
-    player = GameObject.FindWithTag("Player").transform.GetChild(0).gameObject;
+    player = GameObject.FindWithTag("MainCamera");
     obstacles = GameObject.FindGameObjectsWithTag("Enemy");
     // endPoint = GameObject.FindWithTag("Finish");
   }
 
   void Start(){
-    StartCoroutine(SpawnObstaclesCoroutine(new Vector3(0f, 1f, 1f), obstacles[0]));
-    StartCoroutine(SpawnObstaclesCoroutine(new Vector3(1f, 2f, 2f), obstacles[0]));
-    StartCoroutine(SpawnObstaclesCoroutine(new Vector3(2f, 3f, 3f), obstacles[0]));
-    // StartCoroutine(SpawnObstaclesCoroutine(new Vector3(Random.Range(player.transform.localPosition.z, player.transform.localPosition.z + 50f), 2f, 2f), obstacles[1]));
+    // StartCoroutine(SpawnObstaclesCoroutine(new Vector3(0f, 1f, 1f), obstacles[0]));
+    // StartCoroutine(SpawnObstaclesCoroutine(new Vector3(1f, 2f, 2f), obstacles[0]));
+    // StartCoroutine(SpawnObstaclesCoroutine(new Vector3(2f, 3f, 3f), obstacles[0]));
+    StartCoroutine(SpawnObstaclesCoroutine(obstacles[1]));
     // StartCoroutine(SpawnObstaclesCoroutine(new Vector3(Random.Range(player.transform.localPosition.z, player.transform.localPosition.z + 50f), 3f, 3f), obstacles[2]));
   }
 
-  IEnumerator SpawnObstaclesCoroutine (Vector3 spawnPosition, GameObject obstacle)
+  IEnumerator SpawnObstaclesCoroutine (GameObject obstacle)
     {
         WaitForSeconds waitTime = new WaitForSeconds(2);
+        Vector3 spawnPosition = new Vector3(0f, 2f, 1f);
         while (true) {
             Instantiate(obstacle.transform, spawnPosition, Quaternion.identity);
             yield return waitTime;
