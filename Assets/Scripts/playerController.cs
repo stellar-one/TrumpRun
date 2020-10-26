7f8cd093cdcd0c9f13adfcc89d0fb8cff77ff821
JustinNieves94 <51970917+blackmesa94@users.noreply.github.com>
2020-10-26 17:42:44 -0400

    Testing Tun Left

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -12,44 +12,49 @@ public class playerController : MonoBehaviour
     public bool back;
     public bool left;
     public bool right;
-    public Transform L_lane;
-    public Transform M_lane;
-    public Transform R_lane;
     public Animator animator;
-    public GameObject baseMesh;
     public float coins = 0;
     public Text coinText;
-    public GameObject heart;
-    public GameObject heart1;
-    public GameObject heart2;
+    public GameObject baseMesh,heart,heart1,heart2;
     public Transform targetCam;
-    private bool moveleft;
-    private bool moveright;
-
-
-
-
-
+    private bool colidder = false;
+  
+   
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            moveleft = true;
-            moveright = false;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            moveright = true;
-            moveleft = false;  
-        }
-    /*    if (Input.GetKeyUp(KeyCode.D) || (Input.GetKeyUp(KeyCode.A)))
-        {
-            moveright = false;
-            moveleft = false;
-        }
-    */
-
         coinText.text = coins.ToString();
+
+        if (colidder == true)
+        {
+            if ((Input.GetKeyDown(KeyCode.D)))
+            {
+                if (foward)
+                {
+                    Debug.Log("R_Turn");
+                    foward = false;
+                    right = true;
+                    left = false;
+                    back = false;
+                    baseMesh.transform.Rotate(0, 90, 0);
+                    targetCam.transform.Rotate(15, 90, -15);
+                }
+
+            }
+
+            if ((Input.GetKeyDown(KeyCode.A)))
+            {
+                Debug.Log("L_Turn");
+                foward = false;
+                right = false;
+                left = true;
+                back = false;
+                baseMesh.transform.Rotate(0,-90, 0);
+                targetCam.transform.Rotate(15, -90, 15);
+            }
+        }
+
+
+      
         if (foward)
         {
             this.gameObject.transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
@@ -69,6 +74,16 @@ public class playerController : MonoBehaviour
         if (back)
         {
             this.gameObject.transform.Translate(Vector3.back * runSpeed * Time.deltaTime);
+            targetCam.transform.position = new Vector3(baseMesh.transform.position.x - 8f, 6, 0);
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                this.gameObject.transform.Translate(Vector3.right * strafeSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                this.gameObject.transform.Translate(Vector3.left * strafeSpeed * Time.deltaTime);
+            }
         }
 
         if (left)
@@ -99,17 +114,15 @@ public class playerController : MonoBehaviour
             {
                 this.gameObject.transform.Translate(Vector3.back * strafeSpeed * Time.deltaTime);
             }
-
-        }
-
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            this.gameObject.transform.Translate(Vector3.left * runSpeed * Time.deltaTime);
         }
     }
 
     private void OnTriggerEnter(Collider triggered)
     {
+        if (triggered.gameObject.tag == "Right Turn")
+        {
+            colidder = true;
+        }
         if(triggered.gameObject.tag == "Enemy")
         {
             Health = Health - 1f;
@@ -131,68 +144,22 @@ public class playerController : MonoBehaviour
                 animator.SetTrigger("killed");
             }
         }
-
         if(triggered.gameObject.tag == "Coin")
         {
             coins++;
             Destroy(triggered.gameObject);
         }
-
-
-    /*    
-        if(triggered.gameObject.tag == "Right Turn" && moveright)
-        {
-            foward = false;
-            right = true;
-            baseMesh.transform.Rotate( 0, 90, 0);
-            targetCam.transform.Rotate(15, 90,-15);
-        }
-
-        if (triggered.gameObject.tag == "Left Turn" && moveleft)
-        {
-            foward = false;
-            right = false;
-            left = true;
-            baseMesh.transform.Rotate(0, -90, 0);
-            targetCam.transform.Rotate(15, -90, 15);
-        }
-*/
-
-
-
     }
 
-    private void OnTriggerStay(Collider triggered)
+    private void OnTriggerExit(Collider triggered)
     {
-
-        if (triggered.gameObject.tag == "Right Turn" && (Input.GetKeyDown(KeyCode.D)))
+        if (triggered.gameObject.tag == "Right Turn")
         {
-            Debug.Log("Test");
-            foward = false;
-            right = true;
-            baseMesh.transform.Rotate(0, 90, 0);
-            targetCam.transform.Rotate(15, 90, -15);
-        }
-
-        if (triggered.gameObject.tag == "Left Turn" && (Input.GetKeyDown(KeyCode.A)))
-        {
-            foward = false;
-            right = false;
-            left = true;
-            baseMesh.transform.Rotate(0, -90, 0);
-            targetCam.transform.Rotate(15, -90, 15);
+            colidder = false;
         }
     }
 
 
-
-
-
-
-
-
-
-
-
+  
 
 }
