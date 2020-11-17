8edc3c65b8a1647d68a10af5acdd389a8965b829
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2020-11-16 19:40:59 -0500

    restoring level generator progress

--- Assets/LeftRightPath.cs
+++ Assets/LeftRightPath.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class LeftRightPath : Paths
 {
     void Start(){
-        this.x = 650f;
-        this.y = 4f;
-        this.z = 0f;
+        // this.x = 650f;
+        // this.y = 4f;
+        // this.z = 0f;
         float[] leftRight = {-90f, 90f};
         this.transform.Rotate(new Vector3(0f, leftRight[Random.Range(0, leftRight.Length)], 0f));
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -37,19 +37,8 @@ public class GameManager : MonoBehaviour
   IEnumerator generatePath(GameObject[] paths){
     WaitForSeconds waitTime = new WaitForSeconds(15);
     while (true) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-      randomPath = paths[Random.Range(0, paths.Length)];
-      randomPath.GetComponent<Paths>().spawnPath(randomPath);
-=======
       GameObject randomPath = paths[Random.Range(0, paths.Length)];
       randomPath.GetComponent<Paths>().spawnPath(randomPath, randomPath);
->>>>>>> parent of b745d6f... saving progress, see description...
-=======
-      GameObject randomPath = paths[Random.Range(0, paths.Length)];
-      randomPath.GetComponent<Paths>().spawnPath(randomPath, lastPath);
-      lastPath = randomPath;
->>>>>>> parent of 6260745... path generator working, need to work on left/right turns
       yield return waitTime;
     }
   }
